87f1ad53ea9193cea148c55ed761096b9bab4f4c
ibrahim <ibommdv61@gmail.com>
2026-04-17 16:24:09 +0400

    js3

---
+++ app.js
@@ -0,0 +1,16 @@
+ let fullName = "Ibrahim"
+ console.log(fullName.length);
+ console.log(fullName.replaceAll("Ibrahim"," "));
+ 
+
+
+ let firstName = "Ibrahim "
+ let lastName ="Mamedov"
+ console.log( lastName +" "+ firstName)
+ 
+
+
+ let fulllName ="Ibrahim Mamedov"
+ console.log(fulllName[0] + fulllName.split(" ")[1][0]) 
+
+ 
